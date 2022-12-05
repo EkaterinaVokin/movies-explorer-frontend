bd6ab6436931a3b684048c8237f48d57bc335104
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-05 13:53:20 +0700

    feat: Сверстать компонент Portfolio

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,27 @@
+import { Line } from '../Line/Line.js';
+import { Container } from '../Container/Container.js';
+import arrow from '../../images/strelka.svg';
+import './Portfolio.css';
+
+export function Portfolio() {
+  return(
+    <section className="portfolio">
+      <Container main={true}>
+        <h3 className="portfolio__title">Портфолио</h3>
+        <ul className="portfolio__items">
+          <li className="portfolio__item">Статичный сайт
+            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          </li>
+          <Line light={true}/>
+          <li className="portfolio__item">Адаптивный сайт
+            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          </li>
+          <Line light={true}/>
+          <li className="portfolio__item">Одностраничное приложение
+            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          </li>
+        </ul>
+      </Container>
+    </section>
+  ) 
+}
