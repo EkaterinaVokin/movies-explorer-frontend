527df15724609db4347213ebb2803e6ba6fbdedc
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-08 13:05:05 +0700

    fix: Добавить ссылки

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,5 @@
 import { Line } from '../Line/Line.js';
 import { Container } from '../Container/Container.js';
-import arrow from '../../images/strelka.svg';
 import './Portfolio.css';
 
 export function Portfolio() {
@@ -9,16 +8,17 @@ export function Portfolio() {
       <Container type="main">
         <h3 className="portfolio__title">Портфолио</h3>
         <ul className="portfolio__items">
-          <li className="portfolio__item">Статичный сайт
-            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          <li className="portfolio__item">
+            <a className="portfolio__item-link" href="https://code.s3.yandex.net/web-developer/final-projects/project-1/index.html" target="_blank" rel="noreferrer">Статичный сайт
+            </a>  
           </li>
           <Line light={true}/>
-          <li className="portfolio__item">Адаптивный сайт
-            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          <li className="portfolio__item">
+            <a className="portfolio__item-link" href="https://ekaterinavokin.github.io/russian-travel/index.html" target="_blank" rel="noreferrer">Адаптивный сайт</a>
           </li>
           <Line light={true}/>
-          <li className="portfolio__item">Одностраничное приложение
-            <img className="portfolio__img" src={arrow} alt="стрелка"/>
+          <li className="portfolio__item">
+            <a className="portfolio__item-link" href="https://mesto.vokin.nomoredomains.icu/" target="_blank" rel="noreferrer">Одностраничное приложение</a>
           </li>
         </ul>
       </Container>
