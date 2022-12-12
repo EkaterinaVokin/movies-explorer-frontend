3b664a9be335ce85ca300c0b4695d20efebef32a
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-12 17:56:34 +0700

    feat: написать все запросы к нашему  API

---
+++ src/utils/MainApi.js
@@ -0,0 +1,88 @@
+class Api {
+  constructor({ url, headers }) {
+    this._url = url;
+    this._headers = headers;
+  }
+  _request(url, options) {
+    // функция запрос которая принимает два аргумента ссылку и объект опций
+    return fetch(url, {
+      ...options,
+      credentials: 'include',
+    }).then((res) => {
+      return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+  getMe() {
+    // сделать запрос чтобы  получить информацию о пользователе с сервера и токен
+    return this._request(`${this._url}/users/me`, { headers: this._headers });
+  }
+  editProfile({ name, email }) {
+    // создать запрос для редактирование профиля
+    return this._request(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    });
+  }
+  getMovies() {
+    // возвращает все сохранённые текущим  пользователем фильмы
+    return this._request(`${this._url}/movies`,{ headers: this._headers })
+  }
+  addMovies(data) {
+    // создать запрос добавить новый фильм
+    return this._request(`${this._url}/movies`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    });
+  }
+  deleteMovies(id) {
+    // создать запрос чтобы удалить фильм
+    return this._request(`${this._url}/movies/${id}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    });
+  }
+  register({name, email, password}) {
+    // регистрация пользователя
+    return this._request(`${this._url}/signup`,{
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    });
+  }
+  authorize({email, password}) {
+    // авторизация пользователя
+    return this._request(`${this._url}/signin`,{
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+  }
+  logout () {
+    // выход пользователя
+    return this._request(`${this._url}/signout`,{ 
+      method: 'POST',
+      headers: this._headers, 
+    });
+  }
+}
+
+const object = {
+  url: 'https://api.movies.project.nomoredomains.club',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+ export const api = new Api(object);
