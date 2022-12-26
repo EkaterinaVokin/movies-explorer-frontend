c76e185b6709cff0dab444e10dcd258acece64e4
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-26 13:39:27 +0700

    feat: Реализовать фунционал блока поиска фильмов

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,15 @@
 import { SearchForm }  from '../SearchForm/SearchForm.js';
 import { MoviesCardList } from '../MoviesCardList/MoviesCardList.js';
+import { Preloader } from '../Preloader/Preloader.js';
 import './Movies.css';
 
-export function Movies() {
+export function Movies(props) {
+  const { movies, isLoading, onLikeMovie,onDeleteMovie, onSearch, onFilter } = props;
   return(
     <>
-      <SearchForm />
-      <MoviesCardList />
+      <SearchForm name="search-movies" onSearch={onSearch} onFilter={onFilter}/>
+      {isLoading && <Preloader />}
+      <MoviesCardList movies={movies} onDeleteMovie={onDeleteMovie} onLikeMovie={onLikeMovie}/>
     </>
   )
 }
