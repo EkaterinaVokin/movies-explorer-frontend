5aadb08835ded13ba0b13b32defd1ecf6588d9c2
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-26 13:39:45 +0700

    feat: Реализовать фунционал блока поиска сохраненных фильмов

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,12 +1,21 @@
+import React, { useContext } from 'react';
 import { SearchForm }  from '../SearchForm/SearchForm.js';
 import { MoviesCardList } from '../MoviesCardList/MoviesCardList.js';
+import { Preloader } from '../Preloader/Preloader.js';
+import { SavedMoviesContext } from '../../contexts/SavedMoviesContext';
 import './SavedMovies.css';
 
-export function SavedMovies() {
+export function SavedMovies(props) {
+
+  const {saveMovies} = useContext(SavedMoviesContext); // Подписываемся на контекст
+
+  const { isLoading, onDeleteMovie, onSearch, onFilter } = props;
+
   return(
     <>
-    <SearchForm />
-    <MoviesCardList favorite={true}/>
+    <SearchForm name="search-save-movies" onSearch={onSearch} onFilter={onFilter}/>
+    {isLoading && <Preloader />}
+    <MoviesCardList favorite={true} movies={saveMovies} onDeleteMovie={onDeleteMovie}/>
     </>
   )
 }
