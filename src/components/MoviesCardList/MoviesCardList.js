e0f24c3011dca2c16934a46a603be15e7304185f
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-12-26 13:40:24 +0700

    feat: Реализовать фунционал блока отображения фильмов

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,40 +1,89 @@
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { MoviesCard } from '../MoviesCard/MoviesCard';
 import { Container } from '../Container/Container.js';
 import './MoviesCardList.css';
-import Card1 from '../../images/card1.jpg';
-import Card2 from '../../images/card2.jpg';
-import Card3 from '../../images/card3.jpg';
+
 
 export function MoviesCardList(props) {
-  const { favorite } = props;
+  const { favorite, movies, onDeleteMovie, onLikeMovie } = props;
 
   const { pathname } = useLocation();
 
-  const list = [
-    {title: 'В погоне за Бенкси', time: '27 минут', image: Card1},
-    {title: 'В погоне за Бенкси', time: '13 минут', image: Card2},
-    {title: 'В погоне за Бенкси', time: '89 минут', image: Card3},
-    {title: 'В погоне за Бенкси', time: '43 минут', image: Card1},
-  ]
+  const [displayedMovies, setDisplayedMovies] = useState([]); // фильмы, которые будут показаны
+
+  const [hiddenMovies, setHiddenMovies] = useState([]) // скрытые фильмы
+
+  const [count, setCount] = useState(12);
+  const [moreCount, setMoreCount] = useState(3);
+
+  useEffect(() => {
+    onResize()
+    window.addEventListener("resize", onResize)
+
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
+  }, []);
+
+  let timeout = null;
+
+  function onResize() {
+    timeout && clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      const width = window.innerWidth;
+      if(width > 992){
+        setCount(12)
+        setMoreCount(3)
+      } else if(width > 576) {
+        setCount(8)
+        setMoreCount(2)
+      } else {
+        setCount(5)
+        setMoreCount(2)
+      }
+    }, 100);
+  }
+
+  useEffect(() => {
+    setDisplayedMovies(movies.slice(0, count))
+    setHiddenMovies(movies.slice(count))
+  }, [movies, count]);
+
+  function handleMoreClick(){
+    const arrMovies = [
+      ...displayedMovies,
+      ...hiddenMovies.slice(0,moreCount)
+    ];
+    setDisplayedMovies(arrMovies)
+    setHiddenMovies(hiddenMovies.slice(moreCount))
+
+  }
+
+  function getId(movie) {
+    return favorite ? movie.movieId : movie.id
+  }
 
   return(
     <section className={`movies ${pathname !== '/saved-movies' ? '' : 'movies_type_padding'}`}>
       <Container type="movie">
-        {list.length > 0 ? 
+        {displayedMovies.length > 0 ? 
         (<ul className="movies__items">
-          {list.map((item,index) => (
-            <li className="movies__item" key={index}>
-              <MoviesCard 
-                movie={item}
-                removable={favorite}
+          {displayedMovies.map((movie) => (
+            <li className="movies__item" key={getId(movie)}>
+              <MoviesCard
+                movie={movie}
+                favorite={favorite}
+                onDeleteMovie={onDeleteMovie}
+                onLikeMovie={onLikeMovie}
               />
             </li>
           ))}
         </ul>)
         : 
         (<div className="movies__text">Ничего не найдено</div>)}
-        {list.length > 0 && pathname !== '/saved-movies' && (<button className="movies__button" type="button">Ещё</button>)}
+        {displayedMovies.length > 0 && hiddenMovies.length > 0  && (<button className="movies__button" type="button" onClick={handleMoreClick}>Ещё</button>)}
       </Container>
     </section>
   )
